9ce567db08d119f6569828be30df11bf0199b50d
NilsonJTS <nilsonjts@gmail.com>
2024-09-25 21:10:02 -0300

    first commit

---
+++ Conta.cs
@@ -0,0 +1,45 @@
+﻿namespace ExercicioNatal24_09_24;
+
+public class Conta
+{
+    //atribuindo atributos
+    public int numero; 
+    private double saldo;
+    public Cliente cliente;
+/*
+    //getters e setters
+    //==================================
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo=saldo;
+    }
+    //===================================
+*/
+
+    //métodos
+
+    public Conta(int numero, double saldo, Cliente cli){
+        this.numero=numero;
+        Saldo=saldo;
+        this.cliente=cli;
+    }
+
+    public double Saldo{get;set;}
+
+    //métodos
+    public void Creditar(double valor){
+        Saldo+=valor;
+    }
+
+    public void Debitar(double valor){
+        Saldo-=valor;
+    }
+
+    public void MostrarSaldo(){
+         Console.WriteLine($"O saldo atual é: R$ {Saldo}");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using ExercicioNatal24_09_24;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Cliente objCli = new Cliente("Natal", 123);
+        Conta objCon = new Conta(1001, 100, objCli);
+
+
+    int contador=0;
+    while(contador==0){
+            int opcao = 0;
+            Console.WriteLine("Digite 1 para Creditar");
+            Console.WriteLine("Digite 2 para Debitar");
+            Console.WriteLine("Digite 3 para Mostrar Saldo");
+            Console.WriteLine("Digite 0 para sair");
+            opcao=Convert.ToInt16(Console.ReadLine());
+
+            switch(opcao){
+                case 1 : 
+                Console.WriteLine("Digite o valor a ser creditado:");
+                objCon.Creditar(Convert.ToDouble(Console.ReadLine()));
+                break;
+                case 2 :
+                Console.WriteLine("Digite o valor a ser debitado");
+                objCon.Debitar(Convert.ToDouble(Console.ReadLine()));
+                break;
+                case 3 :
+                objCon.MostrarSaldo();
+                break;
+                case 0 :
+                contador=1;
+                break;
+                default:
+                Console.WriteLine("Valor inválido");
+                break;
+            }
+        }
+    }    
+}
